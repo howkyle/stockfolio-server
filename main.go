b474871e711a4b422a2b18665c16c0a8d63e64e2
Kyle Howard <kyle.howard@smsja.net>
2021-09-03 22:35:19 -0500

    inits db and passes to server instance

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "github.com/howkyle/stockfolio-server/server"
+import (
+	"log"
+
+	"github.com/howkyle/stockfolio-server/config"
+	"github.com/howkyle/stockfolio-server/portfolio"
+	"github.com/howkyle/stockfolio-server/server"
+	"github.com/howkyle/stockfolio-server/user"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
-	server := server.Create(":8000")
+	config := config.Config()
+	db := initDB(config.DB)
+	server := server.Create(config.Port, db)
 	server.Start()
+}
+
+func initDB(connection string) *gorm.DB {
+	db, err := gorm.Open(mysql.Open(connection))
+	if err != nil {
+		panic("unable to connect to database")
+	}
+	log.Printf("connected to db: %v", db.Name())
+
+	db.AutoMigrate(user.User{}, portfolio.Portfolio{}, portfolio.Company{})
+
+	return db
 }
--- server/server.go
+++ server/server.go
@@ -7,21 +7,27 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/howkyle/stockfolio-server/analysis"
+	"gorm.io/gorm"
 )
 
 type server struct {
 	port string
+	db   *gorm.DB
 }
 
-func Create(port string) server {
-	return server{port: port}
+//creates server instance
+func Create(port string, db *gorm.DB) server {
+	return server{port, db}
 }
+
+// configures routes and returns router
 func router() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/analyze", analysis.AnalysisHandler()).Methods("POST")
 	return r
 }
 
+//starts the server
 func (s *server) Start() {
 	fmt.Printf("starting server on port %s", s.port)
 	log.Fatal(http.ListenAndServe(s.port, router()))
