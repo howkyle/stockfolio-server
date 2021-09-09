6ab0c2f9f56a66fd01c6bdff635fff04d6356462
Kyle Howard <kyle.howard@smsja.net>
2021-09-09 15:17:28 -0500

    extracts auth into auth interfaces

---
+++ auth/auth.go
@@ -0,0 +1,18 @@
+//handles web app authentication and authorization
+package auth
+
+type AuthManager interface {
+	//accepts username and password and transforms to custom credentials type
+	NewCredentials(username, password string) Credentials
+	//accepts two credential instances, compares equality and returns auth type
+	Authenticate(a, b Credentials) (Auth, error)
+	CheckAuth() error
+}
+type Auth interface {
+	//retrieves string details about authentication jwt, session etc
+	Get() string
+}
+
+//interface to represent credentials
+type Credentials interface {
+}
