91f141d0e4360571db3e17633eb046324ae8bad2
Kyle Howard <kyle.howard@smsja.net>
2021-09-14 23:52:14 -0500

    separates company and porfolio files

--- portfolio/portfolio.go
+++ portfolio/portfolio.go
@@ -1,6 +1,8 @@
 package portfolio
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Dollars float32
 
@@ -10,26 +12,30 @@ type Portfolio struct {
 	Companies []Company
 }
 
-type Company struct {
-	gorm.Model
-	PortfolioID uint
-	Name        string
-	Symbol      string
-	Market      int32
-	Shares      int
-	Price       Dollars
-	Earnings
-	FinacialPosition
+type Service interface {
+	//retrieves a users portfolio
+	Portfolio(userid uint) (Portfolio, error)
+	//adds a new company to the portfolio and returns the company id
+	AddCompany(c Company) (uint, error)
+	//retrieves a company in the users porfolio
+	Company(cid uint) (Company, error)
+	//adds a financial report for a company in the portfolio
+	AddReport(cid uint, r FinancialReport) (uint, error)
+	//retrieves a financial report associated with a company in the portfolio
+	GetReport(rid uint) (FinancialReport, error)
 }
 
-type FinacialPosition struct {
-	CurrentAssets      Dollars
-	CurrentLiabilities Dollars
-	LongAssets         Dollars
-	LongLiabilities    Dollars
-}
-
-type Earnings struct {
-	Income      Dollars
-	Expenditure Dollars
+type Repo interface {
+	//retrieves a portfolio associated with a user
+	Get(userid uint) (Portfolio, error)
+	//inserts a new company linked to a portfolio into the table
+	AddCompany(c Company) (uint, error)
+	//takes a company id and removes the company from the db
+	DeleteCompany(cid uint) error
+	//retrieves a company from the db by id
+	Company(cid uint) (Company, error)
+	//inserts a new report associated with a company
+	AddReport(fr FinancialReport) (uint, error)
+	//retrieves report associated with a company
+	GetReport(rid uint) (FinancialReport, error)
 }
