53ab38677c725f3f30836881b4ae45f216a4d9c5
Kyle Howard <kyle.howard@smsja.net>
2021-09-14 23:53:15 -0500

    registers portfolio service and repo

--- server/server.go
+++ server/server.go
@@ -9,17 +9,19 @@ import (
 	"github.com/howkyle/stockfolio-server/analysis"
 	"github.com/howkyle/stockfolio-server/auth"
 	"github.com/howkyle/stockfolio-server/auth/jwt_auth"
+	"github.com/howkyle/stockfolio-server/portfolio"
 	"github.com/howkyle/stockfolio-server/user"
 	"gorm.io/gorm"
 )
 
 type server struct {
-	port        string
-	router      *mux.Router
-	db          *gorm.DB
-	secret      string
-	userService user.Service
-	authManager auth.AuthManager
+	port             string
+	router           *mux.Router
+	db               *gorm.DB
+	secret           string
+	userService      user.Service
+	portfolioService portfolio.Service
+	authManager      auth.AuthManager
 }
 
 //creates server instance
@@ -36,14 +38,18 @@ func (s *server) configRouter() {
 	r := mux.NewRouter()
 	r.HandleFunc("/login", user.LoginHandler(s.userService)).Methods("POST")
 	r.HandleFunc("/signup", user.SignUpHandler(s.userService)).Methods("POST")
+	r.HandleFunc("/portfolio", s.authManager.Filter(portfolio.GetPortfolioHandler(s.portfolioService))).Methods("GET")
+	r.HandleFunc("/portfolio/add", s.authManager.Filter(portfolio.NewCompanyHandler(s.portfolioService))).Methods("POST")
 	r.HandleFunc("/analyze", s.authManager.Filter(analysis.AnalysisHandler())).Methods("POST")
 	s.router = r
 }
 
 func (s *server) configServices() {
 	ur := user.NewRepository(s.db)
+	pr := portfolio.NewRepository(s.db)
 	s.authManager = jwt_auth.NewJWTAuth(s.secret)
 	s.userService = user.CreateService(ur, s.authManager)
+	s.portfolioService = portfolio.CreateService(pr)
 }
 
 //starts the server
