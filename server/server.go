92024e2e6e0280f73adf1b4559b88dd8d03978ae
Kyle Howard <kyle.howard@smsja.net>
2021-09-16 22:43:55 -0500

    registers company service and repo, updates routes

--- server/server.go
+++ server/server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/howkyle/stockfolio-server/analysis"
 	"github.com/howkyle/stockfolio-server/auth"
 	"github.com/howkyle/stockfolio-server/auth/jwt_auth"
+	"github.com/howkyle/stockfolio-server/company"
 	"github.com/howkyle/stockfolio-server/portfolio"
 	"github.com/howkyle/stockfolio-server/user"
 	"gorm.io/gorm"
@@ -21,6 +22,7 @@ type server struct {
 	secret           string
 	userService      user.Service
 	portfolioService portfolio.Service
+	companyService   company.Service
 	authManager      auth.AuthManager
 }
 
@@ -38,8 +40,10 @@ func (s *server) configRouter() {
 	r := mux.NewRouter()
 	r.HandleFunc("/login", user.LoginHandler(s.userService)).Methods("POST")
 	r.HandleFunc("/signup", user.SignUpHandler(s.userService)).Methods("POST")
+	// r.HandleFunc("/report", s.authManager.Filter(portfolio.NewCompanyHandler(s.portfolioService))).Methods("GET")
+	r.HandleFunc("/report", s.authManager.Filter(company.AddReportHandler(s.companyService))).Methods("POST")
 	r.HandleFunc("/portfolio", s.authManager.Filter(portfolio.GetPortfolioHandler(s.portfolioService))).Methods("GET")
-	r.HandleFunc("/portfolio/add", s.authManager.Filter(portfolio.NewCompanyHandler(s.portfolioService))).Methods("POST")
+	r.HandleFunc("/company/add", s.authManager.Filter(company.NewCompanyHandler(s.companyService))).Methods("POST")
 	r.HandleFunc("/analyze", s.authManager.Filter(analysis.AnalysisHandler())).Methods("POST")
 	s.router = r
 }
@@ -47,8 +51,10 @@ func (s *server) configRouter() {
 func (s *server) configServices() {
 	ur := user.NewRepository(s.db)
 	pr := portfolio.NewRepository(s.db)
+	cr := company.NewRepository(s.db)
 	s.authManager = jwt_auth.NewJWTAuth(s.secret)
 	s.userService = user.CreateService(ur, s.authManager)
+	s.companyService = company.CreateService(cr)
 	s.portfolioService = portfolio.CreateService(pr)
 }
 
