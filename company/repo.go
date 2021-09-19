7032bff17b243259d5d71664504798d7e6de04cb
Kyle Howard <kyle.howard@smsja.net>
2021-09-19 16:28:30 -0500

    implements repo method for companies

--- company/repo.go
+++ company/repo.go
@@ -29,16 +29,30 @@ func (r repository) DeleteCompany(cid uint) error {
 	return nil
 }
 
+//retrieves a company from the db using the company id
 func (r repository) Company(cid uint) (Company, error) {
 	var c Company
 	res := r.db.First(&c, cid)
 	if res.Error != nil {
 		log.Println(res.Error)
-		return Company{}, fmt.Errorf("unable to retrieve company: %v", res.Error)
+		return Company{}, fmt.Errorf("unable to retrieve company: %w", res.Error)
 	}
 	return c, nil
 }
 
+//retrieves a slice of companies from the db using the portfolio id
+func (r repository) Companies(pid uint) ([]Company, error) {
+	var c []Company
+	res := r.db.Where(Company{PortfolioID: pid}).First(&c)
+	if res.Error != nil {
+		log.Println(res.Error)
+		return nil, fmt.Errorf("unable to retrieve companies: %w", res.Error)
+
+	}
+	return c, nil
+}
+
+//inserts a new financial report to the db and return the id
 func (r repository) AddReport(fr FinancialReport) (uint, error) {
 	res := r.db.Create(&fr)
 	if res.Error != nil {
@@ -49,6 +63,7 @@ func (r repository) AddReport(fr FinancialReport) (uint, error) {
 	return fr.ID, nil
 }
 
+//retrieves a financial report from the db using the report id
 func (r repository) GetReport(rid uint) (FinancialReport, error) {
 	var fr FinancialReport
 	res := r.db.First(&fr, rid)
@@ -59,6 +74,7 @@ func (r repository) GetReport(rid uint) (FinancialReport, error) {
 	return fr, nil
 }
 
+//returns a new company repository
 func NewRepository(database *gorm.DB) Repo {
 	return &repository{database}
 }
