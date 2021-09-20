61fece6591c86ebdbdbee49c9d363929523d0c30
Kyle Howard <kyle.howard@smsja.net>
2021-09-19 22:04:26 -0500

    adds handler for report analysis

--- analysis/handler.go
+++ analysis/handler.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/howkyle/stockfolio-server/company"
 )
 
-func AnalysisHandler() http.HandlerFunc {
+func QuickAnalysisHandler(s Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		if r.Body == nil {
@@ -15,7 +17,7 @@ func AnalysisHandler() http.HandlerFunc {
 			return
 		}
 
-		var company Company
+		var company QuickAnalysis
 
 		err := json.NewDecoder(r.Body).Decode(&company)
 		if err != nil {
@@ -24,7 +26,37 @@ func AnalysisHandler() http.HandlerFunc {
 			return
 		}
 
-		result, err := Analyze(&company)
+		result, err := s.Analyze(company.ToFinancialReport())
+		if err != nil {
+			http.Error(w, "unable to run analysis", http.StatusInternalServerError)
+			return
+		}
+
+		response, _ := json.Marshal(result)
+		w.Header().Add("content-type", "application/json")
+		fmt.Fprintf(w, string(response))
+	}
+
+}
+
+func ReportAnalysisHandler(s Service) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Body == nil {
+			http.Error(w, "empty request body", http.StatusBadRequest)
+			return
+		}
+
+		var company company.FinancialReport
+
+		err := json.NewDecoder(r.Body).Decode(&company)
+		if err != nil {
+			log.Printf("unable to decode request body: %v", err)
+			http.Error(w, "unable to decode request body", http.StatusBadRequest)
+			return
+		}
+
+		result, err := s.Analyze(company)
 		if err != nil {
 			http.Error(w, "unable to run analysis", http.StatusInternalServerError)
 			return
