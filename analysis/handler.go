3a7f2ef76f4786665b05a3a620851b87d4ed6616
Kyle Howard <kyle.howard@smsja.net>
2021-09-20 22:41:49 -0500

    creates functions to make reponse and write json

--- analysis/handler.go
+++ analysis/handler.go
@@ -5,25 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/howkyle/stockfolio-server/company"
 )
 
+//handles requests for quick company analysis
 func QuickAnalysisHandler(s Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		if r.Body == nil {
-			http.Error(w, "empty request body", http.StatusBadRequest)
-			return
-		}
-
 		var company QuickAnalysis
-
-		err := json.NewDecoder(r.Body).Decode(&company)
+		err := decodeBody(r, &company)
 		if err != nil {
-			log.Printf("unable to decode request body: %v", err)
-			http.Error(w, "unable to decode request body", http.StatusBadRequest)
-			return
+			http.Error(w, err.Error(), http.StatusBadRequest)
 		}
 
 		result, err := s.Analyze(company.ToFinancialReport())
@@ -32,39 +23,66 @@ func QuickAnalysisHandler(s Service) http.HandlerFunc {
 			return
 		}
 
-		response, _ := json.Marshal(result)
-		w.Header().Add("content-type", "application/json")
-		fmt.Fprintf(w, string(response))
+		response, err := createResponse(result)
+		if err != nil {
+			http.Error(w, "unable to create response", http.StatusInternalServerError)
+		}
+
+		writeJSON(w, response)
 	}
 
 }
 
+//handles requests to analyze a stored financial report
 func ReportAnalysisHandler(s Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		if r.Body == nil {
-			http.Error(w, "empty request body", http.StatusBadRequest)
-			return
-		}
+		var company ReportAnalysis
 
-		var company company.FinancialReport
-
-		err := json.NewDecoder(r.Body).Decode(&company)
+		err := decodeBody(r, &company)
 		if err != nil {
-			log.Printf("unable to decode request body: %v", err)
-			http.Error(w, "unable to decode request body", http.StatusBadRequest)
-			return
+			http.Error(w, err.Error(), http.StatusBadRequest)
 		}
 
-		result, err := s.Analyze(company)
+		result, err := s.Analyze(company.ToFinancialReport())
 		if err != nil {
 			http.Error(w, "unable to run analysis", http.StatusInternalServerError)
 			return
 		}
 
-		response, _ := json.Marshal(result)
-		w.Header().Add("content-type", "application/json")
-		fmt.Fprintf(w, string(response))
+		response, err := createResponse(result)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		writeJSON(w, response)
 	}
 
+}
+
+func decodeBody(r *http.Request, v interface{}) error {
+	if r.Body == nil {
+		return fmt.Errorf("empty request body")
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&v)
+	if err != nil {
+		log.Printf("unable to decode request body: %v", err)
+		return fmt.Errorf("unable to decode request body")
+	}
+
+	return nil
+}
+
+func createResponse(body interface{}) (string, error) {
+	response, err := json.Marshal(body)
+	if err != nil {
+		return "", fmt.Errorf("unable to create response: %v", err)
+	}
+	return string(response), nil
+}
+
+func writeJSON(w http.ResponseWriter, v string) {
+	w.Header().Add("content-type", "application/json")
+	fmt.Fprintf(w, v)
 }
