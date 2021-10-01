ac48a6e4fbeee8659160301ce70ea0510f9ae951
Kyle Howard <kyle.howard@smsja.net>
2021-10-01 12:47:00 -0500

    replace service calls with uman

--- user/handler.go
+++ user/handler.go
@@ -2,27 +2,42 @@ package user
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/howkyle/stockfolio-server/portfolio"
+	"github.com/howkyle/uman"
 )
 
-func SignUpHandler(s Service) http.HandlerFunc {
+func SignUpHandler(u uman.UserManager, a uman.AuthManager) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println("signing up user")
 		var body UserSignup
 		err := json.NewDecoder(r.Body).Decode(&body)
 		if err != nil {
 			http.Error(w, "unable to read payload", http.StatusBadRequest)
+			return
+		}
+		hashedPass, err := uman.NewUserPassCredentials(body.UserName, body.Password).Hash()
+		if err != nil {
+			http.Error(w, "unable to hash credentials", http.StatusInternalServerError)
+			return
 		}
 
-		s.Signup(body)
+		ut := User{Username: body.UserName, Password: hashedPass, Portfolio: portfolio.Portfolio{Title: fmt.Sprintf("%v's Portfolio", body.UserName)}}
+
+		_, err = u.Create(ut)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "unable to create user", http.StatusInternalServerError)
+		}
+
 	}
 }
 
-func LoginHandler(s Service) http.HandlerFunc {
+func LoginHandler(u uman.UserManager, a uman.AuthManager) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println("logging in user")
 		login := struct{ UserName, Password string }{}
 		err := json.NewDecoder(r.Body).Decode(&login)
 		if err != nil {
@@ -30,14 +45,23 @@ func LoginHandler(s Service) http.HandlerFunc {
 			http.Error(w, "bad request", http.StatusBadRequest)
 			return
 		}
-		token, err := s.Login(login.UserName, login.Password)
+
+		user, err := u.Retrieve(User{Username: login.UserName})
 		if err != nil {
-			log.Printf("unable to login:%v", err)
+			log.Println(err)
 			http.Error(w, "failed login", http.StatusUnauthorized)
 			return
-
 		}
-		cookie := http.Cookie{Name: "pyt", Value: token, Domain: "localhost"}
+
+		cred := uman.NewUserPassCredentials(fmt.Sprintf("%v", user.GetID()), user.GetPassword())
+		auth, err := a.Authenticate(cred, login.Password)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "failed login", http.StatusUnauthorized)
+			return
+		}
+
+		cookie := http.Cookie{Name: "pyt", Value: auth.Auth(), Domain: "localhost"}
 		http.SetCookie(w, &cookie)
 	}
 }
